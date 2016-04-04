Correct re-normalisation of the simulated model.
First interesting simulations! Finally!
Cool stuff showing up :)

epidemic_model.py
@@ -10,28 +10,11 @@ import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 
 
-# Scaling
-N = 10000
-# Model parameters
-mu = 0.1
-gamma = 10
-beta = 0.01
-# Fix point
-xstar = gamma / beta
-ystar = mu / beta
-# Initial values
-x0 = N
-y0 = N
-
-
 # Model next step.
-@numba.jit(nopython=True, cache=True)
-def model_next_step(t, x, y):
+@numba.jit(nopython=True)
+def model_next_step(t, x, y, mu, beta, gamma):
 
-    if x == 0:
-        t_death = random.expovariate(gamma*y)
-        return (t+t_death, x, y-1)
-    else:
+    if x > 0 and y > 0:
         t_arrival = random.expovariate(mu*x)
         t_infection = random.expovariate(beta*x*y)
         t_death = random.expovariate(gamma*y)
@@ -42,22 +25,27 @@ def model_next_step(t, x, y):
             return (t+t_infection, x-1, y+1)
         else:
             return (t+t_death, x, y-1)
+    elif y > 0:
+        t_death = random.expovariate(gamma*y)
+        return (t+t_death, x, y-1)
+    else:
+        return(t, x, y)
 
 
 # Simulation of the model
-@numba.jit(nopython=True, cache=True)
-def simulate_model(_x0, _y0, seed=0):
+@numba.jit(nopython=True)
+def simulate_model(_x0, _y0, mu, gamma, beta, seed=0):
     random.seed(int(seed))
     vt = [0.]
     vx = [_x0]
     vy = [_y0]
 
-    (t, x, y) = model_next_step(vt[-1], vx[-1], vy[-1])
+    (t, x, y) = model_next_step(vt[-1], vx[-1], vy[-1], mu, gamma, beta)
     while x > 0 and y > 0:
         vt.append(t)
         vx.append(x)
         vy.append(y)
-        (t, x, y) = model_next_step(vt[-1], vx[-1], vy[-1])
+        (t, x, y) = model_next_step(vt[-1], vx[-1], vy[-1], mu, gamma, beta)
 
     return (np.array(vt), np.array(vx), np.array(vy))
 
@@ -69,8 +57,24 @@ def f_cons_qty(x, y):
     return beta * (x+y) - gamma*math.log(x) - mu*math.log(y)
 vf_cons_qty = np.vectorize(f_cons_qty)
 
+#############################################################################
 # Make a few simulations.
-nb = 1
-N = 10000
-x0 = int(N * xstar)
-y0 = int(N * ystar)
+#############################################################################
+
+# Scaling
+N = 100
+
+# Model parameters
+mu = 0.2
+gamma = 10.
+betap = 0.1
+beta = betap / N
+
+# Fixed point
+xstar = gamma / beta
+ystar = mu / beta
+# Initial values: start from the fixed point.
+x0 = xstar
+y0 = ystar
+
+(vt, vx, vy) = simulate_model(x0, y0, mu, beta, gamma, time.time())